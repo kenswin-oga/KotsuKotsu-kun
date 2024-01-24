4cb6c56a7bed9ae67af6133870af44e0abdd5134
kens <tarotarohanak@gmail.com>
2024-01-25 01:33:37 +0900

    topページデザイン完成

--- src/resources/views/top.blade.php
+++ src/resources/views/top.blade.php
@@ -4,20 +4,38 @@
         @vite(['resources/sass/top.scss'])
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
         <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-
-        <!-- Styles -->
-        <!-- <style>
-        </style> -->
     </head>
-    <body class="antialiased">
-        <div class="test">
-            テスト
+    <body class="page">
+        <div class="header-container">
+            <div class="app-title">コツコツくん</div>
+            <ul class="menu-group">
+                <li class="menu-item">
+                    <a href="#">
+                        ログイン
+                    </a>
+                </li>
+                <li class="menu-item">
+                    <a href="#" class="button">
+                        登録
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <div class="top-content">
+            <div class="sub-title">
+                日々の継続のお供
+            </div>
+            <div class="title">
+                コツコツくん
+            </div>
+            <div class="app-icon">
+                <img src="{{ asset("images/study_human.png") }}" class="app-icon-img1">
+                <img src="{{ asset("images/flag_human.png") }}" class="app-icon-img2">
+            </div>
+            <a href="#" class="button">
+                使ってみる
+            </a>
         </div>
     </body>
 </html>
