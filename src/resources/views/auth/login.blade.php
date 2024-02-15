02e477ac4b78be1e63d0034f1eab3ac45622f4f2
kens <tarotarohanak@gmail.com>
2024-02-16 01:07:14 +0900

    ログイン画面にdeisyuiを適用

--- src/resources/views/auth/login.blade.php
+++ src/resources/views/auth/login.blade.php
@@ -33,8 +33,10 @@
         <form method="POST" action="{{ route('login') }}" class="login-form">
             @csrf
             <div class="form-group">
-                <label for="name">ユーザー名</label>
-                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                <label for="name" class="input input-bordered flex items-center gap-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="w-4 h-4 opacity-70"><path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" /></svg>
+                    <input id="name" type="text" class="grow @error('name') is-invalid @enderror bg-transparent" placeholder="ユーザネーム" name="name" value="{{ old('name') }}" required autofocus/>
+                </label>
                 @error('name')
                     <span class="invalid-feedback" role="alert">
                         <strong>{{ $message }}</strong>
@@ -42,15 +44,17 @@
                 @enderror
             </div>
             <div class="form-group">
-                <label for="password">パスワード</label>
-                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                <label for="password" class="input input-bordered flex items-center gap-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="w-4 h-4 opacity-70"><path fill-rule="evenodd" d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z" clip-rule="evenodd" /></svg>
+                    <input id="password" type="password" class="grow form-control @error('password') is-invalid @enderror bg-transparent" placeholder="パスワード" name="password" required />
+                </label>
                 @error('password')
                     <span class="invalid-feedback" role="alert">
                         <strong>{{ $message }}</strong>
                     </span>
                 @enderror
             </div>
-            <button type="submit" class="button">
+            <button type="submit" class="button btn btn-wide btn-neutral text-lg text-white">
                 はじめる
             </button>
         </form>
