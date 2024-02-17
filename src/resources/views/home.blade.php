52f467b3a02edde9293ddc549adee4971992fa43
kens <tarotarohanak@gmail.com>
2024-02-17 23:23:01 +0900

    マイページレイアウトを変更

--- src/resources/views/home.blade.php
+++ src/resources/views/home.blade.php
@@ -2,6 +2,11 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         @vite(['resources/sass/home.scss'])
+        {{-- <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"> --}}
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Laravel</title>
@@ -38,44 +43,68 @@
             </button>
         </div>
         @endif
-        <div class="container mx-auto px-4 py-6 flex flex-col items-center">
-            <div class="flex flex-col items-center justify-center">
-                <h1 class="text-3xl font-bold mb-4">今日のおすすめ料理</h1>
-                <p class="text-gray-600 mb-6 text-center">好きな料理を選んでレシピを見てみよう！</p>
+        {{-- <div class="hero min-h-screen" style="background-image: url('{{ asset("images/hero_2.png") }}');">
+        </div> --}}
+        
+        <div class="flex m-20">
+            <div class="container mx-auto px-10 w-2/3">
+                <div class="divider" style="border-color: red;"></div> 
+                <div class="flex flex-col items-center justify-center">
+                    <h1 class="text-3xl font-bold mb-4">本日のおすすめ料理</h1>
+                    <p class="text-gray-600 text-center">好きな料理を選んでレシピを見てみよう！</p>
+                </div>
+                <div class="divider"></div> 
+                <div class="bg-base-100">
+                    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-10">
+                        <div class="card card-compact bg-base-100 shadow-xl">
+                            <figure><img src="{{ $recipeDataArray[0]['result'][0]['foodImageUrl'] }}" alt="Recipe 1" class="w-full h-54 object-cover" width="200" height="200" style="aspect-ratio: 200 / 200; object-fit: cover;" /></figure>
+                            <div class="card-body">
+                            <h2 class="card-title">{{ $categoryNames[0] }}</h2>
+                            <div class="card-actions justify-end">
+                                <button class="btn btn-primary">
+                                    <a href="{{ route('recipe-list', ['id' => 0]) }}">レシピ一覧</a>
+                                </button>
+                            </div>
+                            </div>
+                        </div>
+                        <div class="card card-compact bg-base-100 shadow-xl">
+                            <figure><img src="{{ $recipeDataArray[1]['result'][0]['foodImageUrl'] }}" alt="Recipe 1" class="w-full h-54 object-cover" width="200" height="200" style="aspect-ratio: 200 / 200; object-fit: cover;" /></figure>
+                            <div class="card-body">
+                            <h2 class="card-title">{{ $categoryNames[1] }}</h2>
+                            <div class="card-actions justify-end">
+                                <button class="btn btn-primary">
+                                    <a href="{{ route('recipe-list', ['id' => 1]) }}">レシピ一覧</a>
+                                </button>
+                            </div>
+                            </div>
+                        </div>
+                        <div class="card card-compact bg-base-100 shadow-xl">
+                            <figure><img src="{{ $recipeDataArray[2]['result'][0]['foodImageUrl'] }}" alt="Recipe 1" class="w-full h-54 object-cover" width="200" height="200" style="aspect-ratio: 200 / 200; object-fit: cover;" /></figure>
+                            <div class="card-body">
+                            <h2 class="card-title">{{ $categoryNames[2] }}</h2>
+                            <div class="card-actions justify-end">
+                                <button class="btn btn-primary">
+                                    <a href="{{ route('recipe-list', ['id' => 2]) }}">レシピ一覧</a>
+                                </button>
+                            </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <button onclick="reloadPage()" class="inline-flex items-center justify-center mt-4 rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent hover:text-accent-foreground h-10 px-4 py-2">
+                    ほかの料理をみる
+                </button>
+                {{-- <script>
+                    function reloadPage() {
+                        location.reload(true);
+                    }
+                </script> --}}
             </div>
-            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-10">
-                <div class="border rounded-lg overflow-hidden">
-                    <img src="{{ $recipeDataArray[0]['result'][0]['foodImageUrl'] }}" alt="Recipe 1" class="w-full h-54 object-cover" width="200" height="200" style="aspect-ratio: 200 / 200; object-fit: cover;">
-                    <div class="p-4">
-                        <h2 class="font-bold text-lg mb-2">{{ $categoryNames[0] }}</h2>
-                        <button class="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input custom-brown-bg hover:bg-accent text-white h-10 px-4 py-2 mt-4">
-                            <a href="{{ route('recipe-list', ['id' => 0]) }}">レシピ一覧</a>
-                        </button>
-                    </div>
-                </div>
-                <div class="border rounded-lg overflow-hidden">
-                    <img src="{{ $recipeDataArray[1]['result'][0]['foodImageUrl'] }}" alt="Recipe 2" class="w-full h-54 object-cover" width="200" height="200" style="aspect-ratio: 200 / 200; object-fit: cover;">
-                    <div class="p-4">
-                        <h2 class="font-bold text-lg mb-2">{{ $categoryNames[1] }}</h2>
-                        <button class="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input custom-brown-bg hover:bg-brown-700 text-white h-10 px-4 py-2 mt-4">レシピ一覧</button>
-                    </div>
-                </div>
-                <div class="border rounded-lg overflow-hidden">
-                    <img src="{{ $recipeDataArray[2]['result'][0]['foodImageUrl'] }}" alt="Recipe 3" class="w-full h-54 object-cover" width="200" height="200" style="aspect-ratio: 200 / 200; object-fit: cover;">
-                    <div class="p-4">
-                        <h2 class="font-bold text-lg mb-2">{{ $categoryNames[2] }}</h2>
-                        <button class="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input custom-brown-bg hover:bg-accent text-white h-10 px-4 py-2 mt-4">レシピ一覧</button>
-                    </div>
-                </div>
+            <!-- 右側のブロック -->
+            <div class="bg-gray-200 p-6 w-1/3">
+                <h2 class="text-xl font-bold mb-4">新しいコンテンツ</h2>
+                <p>ここに適当なコンテンツを追加します。</p>
             </div>
-            <button onclick="reloadPage()" class="inline-flex items-center justify-center mt-4 rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent hover:text-accent-foreground h-10 px-4 py-2">
-                ほかの料理をみる
-            </button>
-            {{-- <script>
-                function reloadPage() {
-                    location.reload(true);
-                }
-            </script> --}}
         </div>
     </body>
 </html>
