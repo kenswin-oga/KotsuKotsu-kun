c268cd5f5d089825ba09acf20f5bfefdbe6eb193
kens <tarotarohanak@gmail.com>
2024-02-25 22:12:05 +0900

    ハンバーガーメニューに背景色を追加

--- src/resources/views/auth/login.blade.php
+++ src/resources/views/auth/login.blade.php
@@ -13,7 +13,7 @@
                 <div tabindex="0" role="button" class="btn btn-ghost btn-circle">
                   <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
                 </div>
-                <ul tabindex="0" class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow rounded-box w-32">
+                <ul tabindex="0" class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow rounded-box w-32"style="background-color: #ffedd5;">
                   <li><a href="/login">マイページ</a></li>
                   <li><a href="/register">登録</a></li>
                 </ul>
